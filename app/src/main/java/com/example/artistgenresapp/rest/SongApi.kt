07ea3a5a4614ec6b3282c6eab33bc2ea2606cee4
DanGugly <elijahmckoy1997@hotmail.com>
2021-12-09 00:21:50 -0500

    Add API class

---
+++ app/src/main/java/com/example/artistgenresapp/rest/SongApi.kt
@@ -0,0 +1,30 @@
+package com.example.artistgenresapp.rest
+
+import com.example.artistgenresapp.model.Song
+import io.reactivex.Single
+import retrofit2.http.GET
+import retrofit2.http.Query
+
+interface SongApi {
+
+    @GET(SEARCH)
+    fun getSongs(
+        @Query("term") genre : String,
+        @Query("amp;media") media : String = SONG_MEDIA,
+        @Query("amp;entity") entity : String = SONG_ENTITY,
+        @Query("amp;limit") limit : String = SONG_LIMIT,
+
+    ) : Single<Song>
+
+    companion object{
+        private const val SONG_CLASSICK = "classick"
+        private const val SONG_POP = "pop"
+        private const val SONG_ROCK = "rock"
+        private const val SONG_MEDIA = "music"
+        private const val SONG_ENTITY = "song"
+        private const val SONG_LIMIT = "50"
+        const val END = "&amp;media=music&amp;entity=song&amp;limit=50"
+        const val SEARCH = "search"
+        const val BASE_URL = "https://itunes.apple.com/"
+    }
+}
