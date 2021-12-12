24a7c4aa27245431c4968d9c97a750a76cde52f2
DanGugly <elijahmckoy1997@hotmail.com>
2021-12-11 22:19:32 -0500

    Update Rock UI

--- app/src/main/java/com/example/artistgenresapp/adapter/RockAdapter.kt
+++ app/src/main/java/com/example/artistgenresapp/adapter/RockAdapter.kt
@@ -3,10 +3,12 @@ package com.example.artistgenresapp.adapter
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.example.artistgenresapp.R
 import com.example.artistgenresapp.model.Result
+import com.squareup.picasso.Picasso
 
 class RockAdapter(
     private val rockList: MutableList<Result> = mutableListOf()
@@ -29,7 +31,19 @@ class RockAdapter(
 
     override fun onBindViewHolder(holder: RockAdapterViewHolder, position: Int) {
         val song = rockList[position]
-        holder.songName.text = song.trackName
+        holder.artistName.text = song.artistName
+        holder.trackPrice.text = song.trackPrice.toString()+" "+ song.currency
+        holder.collectionName.text = song.collectionName
+        getWeatherIcon(song.artworkUrl60, holder.artwork)
+    }
+
+    private fun getWeatherIcon(path:String, artwork: ImageView){
+        Picasso
+            .get()
+            .load(path)
+            .resize(250,250)
+            //.centerCrop()
+            .into(artwork)
     }
 
     override fun getItemCount(): Int = rockList.size
@@ -37,5 +51,8 @@ class RockAdapter(
 }
 
 class RockAdapterViewHolder(itemView: View): RecyclerView.ViewHolder(itemView){
-    var songName : TextView = itemView.findViewById(R.id.song_name)
+    var artistName : TextView = itemView.findViewById(R.id.artistName)
+    var collectionName : TextView = itemView.findViewById(R.id.collectionName)
+    var artwork : ImageView = itemView.findViewById(R.id.artwork)
+    var trackPrice : TextView = itemView.findViewById(R.id.trackPrice)
 }
